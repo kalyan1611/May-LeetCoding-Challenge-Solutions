111a317b036c16fcb0b86f227a97b0e4ce78a831
kalyan.cherukuru <30562592+kalyan1611@users.noreply.github.com>
2020-05-06 22:56:02 +0530

    Update NumberComplement.java

--- NumberComplement.java
+++ NumberComplement.java
@@ -1,9 +1,12 @@
 class Solution {
     public int findComplement(int num) {
+        if (num == 0) {
+            return 1;
+        }
         int num2 = 0;
         int exponent = 0;
         while (num > 0) {
-            num2 += ((num%2==0)?1:0)*(int)(Math.pow(2,exponent++));
+            num2 += (1-num%2)*(int)(Math.pow(2,exponent++));
             num /= 2;
         }
         return num2;

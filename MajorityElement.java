d2e0f3a157fbd3c5d198ae47adebbc765c62d6a1
kalyan.cherukuru <30562592+kalyan1611@users.noreply.github.com>
2020-05-22 10:35:17 +0530

    Update MajorityElement.java

--- MajorityElement.java
+++ MajorityElement.java
@@ -2,13 +2,13 @@ class Solution {
     public int majorityElement(int[] nums) {
         int leader = nums[0];
         int votes = 1;
-        for (int i=1; i<nums.length; i++) {
-            if (nums[i]==leader) {
-                votes++;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == leader) {
+                ++votes;
             } else {
-                votes--;
+                --votes;
             }
-            if (votes==0) {
+            if (votes == 0) {
                 leader = nums[i];
                 votes = 1;
             }

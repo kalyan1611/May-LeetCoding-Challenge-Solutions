6149d1fa71530a0fdb78be676cb2bd4248fed812
kalyan.cherukuru <30562592+kalyan1611@users.noreply.github.com>
2020-05-25 16:47:26 +0530

    Update UncrossedLines.java

--- UncrossedLines.java
+++ UncrossedLines.java
@@ -5,17 +5,17 @@ class Solution {
         int a = A.length;
         int b = B.length;
         int[][] dp = new int[a + 1][b + 1];
-        
-        for (int i = 1, a_i = i - 1; a_i < a; i++, a_i++) {
-            for (int j = 1, b_j = j - 1; b_j < b; j++, b_j++) {
-                if (A[a_i] == B[b_j]) {
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= b; j++) {
+                if (A[i - 1] == B[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        
+
         return dp[a][b];
     }
 }
